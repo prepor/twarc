1aaef44df92c59118ce0e54825ae5561ad7d0549
prepor <ceo@prepor.ru>
2020-05-28 15:56:09 +0200

    bump version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject twarc "0.1.13"
+(defproject twarc "0.1.14"
   :description "Doing Quartz the right way"
   :url "https://github.com/prepor/twarc"
   :license {:name "Eclipse Public License",
